1a27f013f6b2c9deeab2b22456f619932b25bdce
Alex Gavalas <alex.gavalas.92@gmail.com>
2024-12-04 13:51:00 +0200

    refactor(day3): extracts some functions

--- day3/Main.hs
+++ day3/Main.hs
@@ -2,38 +2,35 @@ module Main where
 
 import Text.Regex.PCRE ((=~))
 
+isDont :: String -> Bool
+isDont = (== "don't()")
+
+isDo :: String -> Bool
+isDo = (== "do()")
+
+performCalculations :: (Bool, Int) -> String -> (Bool, Int)
+performCalculations (shouldProcessItem, acc) item
+  | isDont item = (False, acc)
+  | isDo item = (True, acc)
+  | otherwise =
+      let [n1, n2] = concatMap (tail . map read) (item =~ "(\\d+),(\\d+)" :: [[String]])
+       in (shouldProcessItem, acc + if shouldProcessItem then n1 * n2 else 0)
+
 main :: IO ()
 main = do
   putStrLn "Day 3, Part 1"
 
   contents <- readFile "./day3/input.txt"
 
-  let matches = contents =~ "mul\\(\\d+,\\d+\\)" :: [[String]]
-      multiplications = map tail $ concatMap (\item -> item =~ "(\\d+),(\\d+)" :: [[String]]) (concat matches)
+  let matches = concat (contents =~ "mul\\(\\d+,\\d+\\)" :: [[String]])
+      multiplications = map tail $ concatMap (\item -> item =~ "(\\d+),(\\d+)" :: [[String]]) matches
       result = sum $ map (\[x, y] -> read x * read y) multiplications
 
   putStrLn $ "Part 1 answer (182619815): " ++ show result
 
   putStrLn "\nDay 3, Part 2"
 
-  let matches = contents =~ "mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)" :: [[String]]
-      isDont = (== "don't()")
-      isDo = (== "do()")
-      (_, multiplicationsRes) =
-        foldl
-          ( \(shouldCalc, acc) x -> do
-              if isDont x
-                then (False, acc)
-                else
-                  if isDo x
-                    then (True, acc)
-                    else do
-                      let [[xS, yS]] = map tail (x =~ "(\\d+),(\\d+)" :: [[String]])
-                          xN = read xS :: Int
-                          yN = read yS :: Int
-                      (shouldCalc, if shouldCalc then acc + (xN * yN) else acc)
-          )
-          (True, 0)
-          $ concat matches
+  let matches = concat (contents =~ "mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)" :: [[String]])
+      (_, result) = foldl performCalculations (True, 0) matches
 
-  putStrLn $ "Part 3 answer (80747545): " ++ show multiplicationsRes
+  putStrLn $ "Part 3 answer (80747545): " ++ show result
